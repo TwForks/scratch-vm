d935e584a559256689d70e37902f120cddfa1844
yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>
2025-05-20 09:45:27 -0400

    Add Scratch.extensions.worker

--- src/extension-support/extension-worker.js
+++ src/extension-support/extension-worker.js
@@ -96,6 +96,7 @@ Object.assign(global.Scratch, ScratchCommon, {
  */
 const extensionWorker = new ExtensionWorker();
 global.Scratch.extensions = {
+    worker: true, // TW: To match the other stuff like unsandboxed.
     register: extensionWorker.register.bind(extensionWorker)
 };
 
